fc47c09343d6fe9b5703aca1f8ed40baf246d23c
Shashank Kumar <shashankkumar922@gmail.com>
2024-03-23 16:39:38 +0530

    initial commit

---
+++ src/main/java/com/shashank/aarambh/AarambhApplication.java
@@ -0,0 +1,13 @@
+package com.shashank.aarambh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AarambhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AarambhApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shashank/aarambh/AarambhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shashank.aarambh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AarambhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
